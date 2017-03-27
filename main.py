d50a172275b40222d7fc675d742e974a4acc0e26
Alex Hochheiden <alexander.hochheiden@uleth.ca>
2017-03-27 08:11:10 -0600

    Removed 2nd argument from main.py and truncated the .kiss2 extension from the input file, which is then used for all subsequent arguments.

--- main.py
+++ main.py
@@ -1,31 +1,36 @@
 import sys
 import os
+import os.path
 from subprocess import call
 
 ## Constants
 ESOP_EXT = ".esop"
 PLA_EXT = ".pla"
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 2:
     print "Usage:"
-    print "python main.py <input-kiss2-file> <output name>"
+    print "python main.py <input-kiss2-file>"
     sys.exit(1)
 
 # ## Store arguments
 inputKiss2 = sys.argv[1]
-output = sys.argv[2]
+
+tempPath = os.path.split(inputKiss2)[1]
+inputFileNameWithExtensionRemoved = os.path.splitext(tempPath)[0]
+
+print inputFileNameWithExtensionRemoved
 
 ## First, we convert the input kiss2 file to a .pla
 call("python KissToPLA_XOR.py %s %s" %
-    (inputKiss2,output), shell = True)
+    (inputKiss2, inputFileNameWithExtensionRemoved), shell = True)
 
 ## Second, pass .pla file through exorcism_org
 call("./exorcism_org %s" %
-    output + PLA_EXT, shell = True)
+    inputFileNameWithExtensionRemoved + PLA_EXT, shell = True)
 
 
 ## Call shared cubed synthesis program and output to a metrics text file
-call("./GetGateCount.sh %s" % output + ESOP_EXT, shell = True)
+call("./GetGateCount.sh %s" % inputFileNameWithExtensionRemoved + ESOP_EXT, shell = True)
 
 ## Clean up
-os.remove(output + PLA_EXT)
+os.remove(inputFileNameWithExtensionRemoved + PLA_EXT)
