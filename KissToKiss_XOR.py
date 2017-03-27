55639dfef52075f23177ce54dac80a6560495522
Micah Tigley <tigleym@gmail.com>
2017-03-27 14:47:01 -0600

    Update main.py to use new Kiss scripts

--- KissToKiss_XOR.py
+++ KissToKiss_XOR.py
@@ -1,16 +1,16 @@
 import re
 import math
+import os
 import sys
 
 if len(sys.argv) != 2:
  print "Usage: \n"
- print "python KissToPLA_XOR.py <input kiss2 file>"
+ print "python KissToKiss_XOR.py <input kiss2 file>"
  sys.exit(1)
 
-
-filename = re.sub('\.kiss2$','',sys.argv[1])
-fileoutput = filename + "_ModNextState.kiss2"
-
+splitPath = os.path.split(sys.argv[1])[1]
+filename = os.path.splitext(splitPath)[0]
+fileoutput = filename + ".kiss2"
 
 fin=open(sys.argv[1],'r')
 fout=open(fileoutput, 'w')
@@ -31,10 +31,8 @@ fout.write(s)
 for line in fin:
     tmp = re.sub("[^\w,-]"," ",line).split()#tmp will now contain input state1, state2, output,
     tmp[1]  = re.findall('\d+', tmp[1])[0]
-    tmp[2] = re.findall('\d+',tmp[2])[0]#pulling the digits out of tmp2 and tmp1 
+    tmp[2] = re.findall('\d+',tmp[2])[0]#pulling the digits out of tmp2 and tmp1
     modifiedQ = int(tmp[1]) ^ int(tmp[2])
     tmp[2] = str(modifiedQ);
     tmp.append('\n')
     fout.write(" ".join(tmp));
-
-    
--- main.py
+++ main.py
@@ -1,34 +1,48 @@
 import sys
 import os
-import os.path
 from subprocess import call
 
 ## Constants
 ESOP_EXT = ".esop"
+KISS2_EXT = ".kiss2"
 PLA_EXT = ".pla"
+TXT_EXT = ".txt"
+
+OUTPUT_1 = "outputs/step1_Kiss2ToKiss2XOR/"
+OUTPUT_2 = "outputs/step2_Kiss2XORToPLA/"
+OUTPUT_3 = "outputs/step3_PLAToESOP/"
+OUTPUT_4="outputs/step4_metrics/"
 
 if len(sys.argv) != 2:
     print "Usage:"
     print "python main.py <input-kiss2-file>"
     sys.exit(1)
 
-# ## Store arguments
+## Store arguments
 inputKiss2 = sys.argv[1]
 
 tempPath = os.path.split(inputKiss2)[1]
 inputFileNameWithExtensionRemoved = os.path.splitext(tempPath)[0]
 
-## First, we convert the input kiss2 file to a .pla
-call("python KissToPLA_XOR.py %s %s" %
-    (inputKiss2, inputFileNameWithExtensionRemoved), shell = True)
+## First, we convert the input kiss2 file to kiss2 with modified states
+call("python KissToKiss_XOR.py %s" %
+    inputKiss2, shell = True)
 
-## Second, pass .pla file through exorcism_org
+## Second, we convert the modified kiss2 file to .pla
+call("python KissToPLA.py %s %s" %
+    (inputFileNameWithExtensionRemoved + KISS2_EXT, inputFileNameWithExtensionRemoved), shell = True)
+
+## Third, pass .pla file through exorcism_org
 call("./exorcism_org %s" %
     inputFileNameWithExtensionRemoved + PLA_EXT, shell = True)
+## Note: the .esop file is consumed by the shared cube synthesis program, so we make a
+## copy of the file to the OUTPUT_3 folder in this step.
+call("cp " + inputFileNameWithExtensionRemoved + ESOP_EXT + " " + OUTPUT_3, shell = True)
 
-
-## Call shared cubed synthesis program and output to a metrics text file
+# Fourth, call shared cubed synthesis program and output to a metrics text file
 call("./GetGateCount.sh %s" % inputFileNameWithExtensionRemoved + ESOP_EXT, shell = True)
 
-## Clean up
-os.remove(inputFileNameWithExtensionRemoved + PLA_EXT)
+## Move intermediate files to outputs folder,
+call("mv " + inputFileNameWithExtensionRemoved + KISS2_EXT + " " + OUTPUT_1, shell = True)
+call("mv " + inputFileNameWithExtensionRemoved + PLA_EXT + " " + OUTPUT_2, shell = True)
+call("mv " + inputFileNameWithExtensionRemoved + TXT_EXT + " " + OUTPUT_4, shell = True)
