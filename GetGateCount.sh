fea712c173210f255226bdcd89a531b6a5e49d81
Micah Tigley <tigleym@gmail.com>
2017-03-29 20:27:56 -0600

    Update main.py to iterate file in inputs and generate a metrics txt for each

--- GetGateCount.sh
+++ GetGateCount.sh
@@ -7,25 +7,23 @@ TXT_LIST="esop_file_list.txt"
 mkdir $DIR
 touch $TXT_LIST
 
+file=$1
+metricsFilename=${file%.*}
+
 ## Move the esop file to esop_files_w folder
 ## and write each file to esop_file_list.txt
 mv $1 $DIR
 
 for file in "$DIR"/*
 do
-  echo "Filename: $file"
   echo $file | cut -d'/' -f 2 &> $TXT_LIST
 done
 
 
 ## Execute Improved_Shared_Cube_Synthesis program
-echo "Gathering gate count from .esop files..."
+echo "Gathering gate count from $metricsFilename.esop file..."
 
-file=$1
-metricsFilename=${file%.*}
 java -jar ./Improved_Shared_Cube_Synthesis.jar &>  "$metricsFilename.txt"
-
-echo "Finished shared cube synthesis"
 
 ## Clean up
 rm -rf $DIR
--- main.py
+++ main.py
@@ -15,36 +15,36 @@ OUTPUT_2 = "outputs/step2_Kiss2XORToPLA/"
 OUTPUT_3 = "outputs/step3_PLAToESOP/"
 OUTPUT_4="outputs/step4_metrics/"
 
-if len(sys.argv) != 2:
-    print "Usage:"
-    print "python main.py <input-kiss2-file>"
-    sys.exit(1)
+## Iterate each .kiss2 file in the inputs directory and generate a metrics file
+for filename in os.listdir(INPUTS_DIR):
+    if filename.endswith(".kiss2"):
 
-## Store arguments
-inputKiss2 = INPUTS_DIR + sys.argv[1]
+        tempPath = os.path.split(filename)[1]
+        inputFileNameWithExtensionRemoved = os.path.splitext(tempPath)[0]
 
-tempPath = os.path.split(inputKiss2)[1]
-inputFileNameWithExtensionRemoved = os.path.splitext(tempPath)[0]
+        ## First, we convert the input kiss2 file to kiss2 with modified states
+        call("python KissToKiss_XOR.py %s" %
+            INPUTS_DIR + filename, shell = True)
 
-## First, we convert the input kiss2 file to kiss2 with modified states
-call("python KissToKiss_XOR.py %s" %
-    inputKiss2, shell = True)
+        ## Second, we convert the modified kiss2 file to .pla
+        call("python KissToPLA.py %s %s" %
+            (inputFileNameWithExtensionRemoved + KISS2_EXT, inputFileNameWithExtensionRemoved), shell = True)
 
-## Second, we convert the modified kiss2 file to .pla
-call("python KissToPLA.py %s %s" %
-    (inputFileNameWithExtensionRemoved + KISS2_EXT, inputFileNameWithExtensionRemoved), shell = True)
+        ## Third, pass .pla file through exorcism_org
+        call("./exorcism_org %s" %
+            inputFileNameWithExtensionRemoved + PLA_EXT, shell = True)
+        ## Note: the .esop file is consumed by the shared cube synthesis program, so we make a
+        ## copy of the file to the OUTPUT_3 folder in this step.
+        call("cp " + inputFileNameWithExtensionRemoved + ESOP_EXT + " " + OUTPUT_3, shell = True)
 
-## Third, pass .pla file through exorcism_org
-call("./exorcism_org %s" %
-    inputFileNameWithExtensionRemoved + PLA_EXT, shell = True)
-## Note: the .esop file is consumed by the shared cube synthesis program, so we make a
-## copy of the file to the OUTPUT_3 folder in this step.
-call("cp " + inputFileNameWithExtensionRemoved + ESOP_EXT + " " + OUTPUT_3, shell = True)
+        # Fourth, call shared cubed synthesis program and output to a metrics text file
+        call("./GetGateCount.sh %s" % inputFileNameWithExtensionRemoved + ESOP_EXT, shell = True)
 
-# Fourth, call shared cubed synthesis program and output to a metrics text file
-call("./GetGateCount.sh %s" % inputFileNameWithExtensionRemoved + ESOP_EXT, shell = True)
+        ## Move intermediate files to outputs folder,
+        call("mv " + inputFileNameWithExtensionRemoved + KISS2_EXT + " " + OUTPUT_1, shell = True)
+        call("mv " + inputFileNameWithExtensionRemoved + PLA_EXT + " " + OUTPUT_2, shell = True)
+        call("mv " + inputFileNameWithExtensionRemoved + TXT_EXT + " " + OUTPUT_4, shell = True)
 
-## Move intermediate files to outputs folder,
-call("mv " + inputFileNameWithExtensionRemoved + KISS2_EXT + " " + OUTPUT_1, shell = True)
-call("mv " + inputFileNameWithExtensionRemoved + PLA_EXT + " " + OUTPUT_2, shell = True)
-call("mv " + inputFileNameWithExtensionRemoved + TXT_EXT + " " + OUTPUT_4, shell = True)
+        continue
+    else:
+        continue
